8a2423cd88911a7f8ee38adde76261111b230bab
DESKTOP-A8HQHM9\Admin <hobaotoan10aa2@gmail.com>
2024-12-15 13:08:02 +0700

    Re-structure about the code

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,3 +1,24 @@
+WITH dim_product__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+)
+
+, dim_product__rename_column AS(
+  SELECT stock_item_id AS product_key
+  , stock_item_name AS product_name
+  , brand AS brand_name
+  FROM dim_product__source
+)
+
+, dim_product__cast_type AS (
+  SELECT CAST(product_key AS INTEGER) AS product_key
+  , CAST(product_name AS STRING) AS product_name
+  , CAST(brand_name AS STRING) AS brand_name
+  FROM dim_product__rename_column
+)
+
 SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+  product_key
+  , product_name
+  , brand_name
+FROM dim_product__cast_type
--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,5 +1,36 @@
-SELECT 
-  order_line_id AS sales_order_line_key,
-  quantity AS quantity,
-  unit_price AS unit_price
-FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+WITH fact_sales_order_line__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+)
+
+, fact_sales_order_line__rename AS (
+  SELECT 
+  order_line_id AS sales_order_line_key
+  , stock_item_id AS product_key
+  , quantity AS quantity
+  , unit_price AS unit_price
+  FROM fact_sales_order_line__source
+)
+
+, fact_sales_order_line__cast_type AS(
+  SELECT 
+  CAST(sales_order_line_key AS INTEGER) AS sales_order_line_key
+  , CAST(product_key AS INTEGER) AS product_key
+  , CAST(quantity AS INTEGER) AS quantity
+  , CAST(unit_price AS NUMERIC) AS unit_price
+
+  FROM
+  fact_sales_order_line__rename
+)
+
+,fact_sales_order_line__calculated_measure AS(
+  *
+  , quantity * unit_price AS gross_amount
+)
+
+SELECT sales_order_line_key
+, product_key
+, quantity
+, unit_price
+, gross_amount
+FROM fact_sales_order_line__cast_type
